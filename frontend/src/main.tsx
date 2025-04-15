ca57c2898e45fd2058ef393097d6328ce209f935
Alex <situalex123@gmail.com>
2025-04-15 03:16:20 -0700

    added login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
-
-import { Router } from './router.tsx'
-import DVDScreensaver from './components/DVDScreensaver.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-    <Router>
-    </Router>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
